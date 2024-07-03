7499d6a6c68fae64d017e4808cf8e5531f982c70
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-07-03 20:06:23 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Nav from "../components/Nav";
+
+
+const Home = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            <Nav></Nav>
+            <h2>Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
